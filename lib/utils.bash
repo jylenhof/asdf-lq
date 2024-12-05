e3c8f04b78e5c1ac0792cb2d4f777d09ff6dbf56
Jean-Yves LENHOF <jygithub@lenhof.eu.org>
2024-12-05 23:37:00 +0100

    chore: update some TODO

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for lq.
 GH_REPO="https://github.com/clux/lq"
 TOOL_NAME="lq"
 TOOL_TEST="lq --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if lq has other means of determining installable versions.
 	list_github_tags
 }
 
@@ -89,7 +86,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert lq executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
