3327fe3f2cea5c68a081fb6b413b0925f5ccc156
jylenhof <jygithub@lenhof.eu.org>
2025-11-24 21:48:57 +0100

    depending on version

--- lib/utils.bash
+++ lib/utils.bash
@@ -68,7 +68,11 @@ download_release() {
 	arch="$(get_arch)"
 	os="$(get_os)"
 
-	url="$GH_REPO/releases/${version}/download/lq-${arch}-${os}.tar.xz"
+	if [ "${version}" == "latest" ]; then
+		url="$GH_REPO/releases/${version}/download/lq-${arch}-${os}.tar.xz"
+	else
+		url="$GH_REPO/releases/download/${version}/lq-${arch}-${os}.tar.xz"
+	fi
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
